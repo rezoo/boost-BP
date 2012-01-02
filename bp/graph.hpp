0f83a5939e9ab61651dbfb002bc6ca238c82c706
Masaki Saito <rezoolab@gmail.com>
2012-01-02 22:09:46 +0900

    use visitor to make more generically.

--- bp/graph.hpp
+++ bp/graph.hpp
@@ -2,18 +2,113 @@
 
 #include <utility>
 #include <iterator>
+#include <vector>
 
 #include <boost/optional.hpp>
 #include <boost/graph/graph_traits.hpp>
 
+enum edge_message_t { edge_message };
+enum vertex_belief_t { vertex_belief };
+namespace boost {
+    BOOST_INSTALL_PROPERTY(edge, message);
+    BOOST_INSTALL_PROPERTY(vertex, belief);
+}
+
 namespace bp {
 
+template<typename Graph, typename Visitor>
+void sum_product(Graph& graph, Visitor visitor) {
+    typedef boost::graph_traits<Graph> Traits;
+    typedef typename Traits::vertex_descriptor Vertex;
+    typedef typename Traits::edge_descriptor Edge;
+    typedef typename Traits::edges_size_type EdgesSizeType;
+
+    // initialize messages
+    visitor.init_messages(graph);
+
+    // make messages
+    EdgesSizeType n_unknown_messages = boost::num_edges(graph);
+    typedef std::vector<Edge> EdgeVector;
+    typedef typename EdgeVector::iterator EdgeIterator;
+    EdgeVector unknown_in_edges;
+    EdgeVector known_in_edges;
+    EdgeVector unknown_out_edges;
+    while(n_unknown_messages > 0) {
+        typename Traits::vertex_iterator vi, vi_end;
+        boost::tie(vi, vi_end) = boost::vertices(graph);
+        for(; vi != vi_end; ++vi) {
+            unknown_in_edges.clear();
+            known_in_edges.clear();
+            unknown_out_edges.clear();
+
+            typename Traits::in_edge_iterator ein, ein_end;
+            boost::tie(ein, ein_end) =
+                boost::in_edges(*vi, graph);
+            for(; ein != ein_end; ++ein) {
+                if(visitor.is_initialized(*ein, graph)) {
+                    known_in_edges.push_back(*ein);
+                } else {
+                    unknown_in_edges.push_back(*ein);
+                }
+            }
+            typename Traits::out_edge_iterator eout, eout_end;
+            boost::tie(eout, eout_end) =
+                boost::out_edges(*vi, graph);
+            for(; eout != eout_end; ++eout) {
+                if(!visitor.is_initialized(*eout, graph))
+                    unknown_out_edges.push_back(*eout);
+            }
+
+            if(unknown_in_edges.size() == 0) {
+                EdgeIterator it = unknown_out_edges.begin();
+                EdgeIterator it_end = unknown_out_edges.end();
+                for(; it != it_end; ++it) {
+                    visitor.make_message(
+                        *it,
+                        known_in_edges.begin(),
+                        known_in_edges.end(),
+                        graph);
+                    n_unknown_messages--;
+                }
+            } else if(unknown_in_edges.size() == 1) {
+                Vertex target =
+                    boost::source(unknown_in_edges[0], graph);
+                EdgeIterator it = unknown_out_edges.begin();
+                EdgeIterator it_end = unknown_out_edges.end();
+                for(; it != it_end; ++it) {
+                    if(target == boost::target(*it, graph)) {
+                        visitor.make_message(
+                            *it,
+                            known_in_edges.begin(),
+                            known_in_edges.end(),
+                            graph);
+                        n_unknown_messages--;
+                        break;
+                    }
+                }
+            } // end if statement
+        } // end vertex loop
+    } // end message loop
+
+    // make belief
+    typename Traits::vertex_iterator vi, vi_end;
+    boost::tie(vi, vi_end) = boost::vertices(graph);
+    for(; vi != vi_end; ++vi) {
+        typename Traits::in_edge_iterator ein, ein_end;
+        boost::tie(ein, ein_end) = boost::in_edges(*vi, graph);
+        visitor.make_belief(*vi, ein, ein_end, graph);
+    }
+}
+
+/*
 template<typename Message,
          typename Graph,
          typename OutputIterator,
+         typename PropertyMap,
          typename Visitor>
 void sum_product(const Graph& graph,
                  OutputIterator belief,
+                 PropertyMap& message_map,
                  Visitor visitor) {
     typedef boost::graph_traits<Graph> Traits;
     typedef typename Traits::vertex_descriptor Vertex;
@@ -33,6 +128,7 @@ void sum_product(const Graph& graph,
     MessageVector messages(boost::num_edges(graph), OMessage());
     MessageMap message_map(messages.begin(), edge_map);
 
+    // make messages
     size_t n_unknown_messages = boost::num_edges(graph);
     typedef std::vector<Edge> EdgeVector;
     typedef typename EdgeVector::iterator EdgeIterator;
@@ -40,9 +136,9 @@ void sum_product(const Graph& graph,
     EdgeVector known_in_edges;
     EdgeVector unknown_out_edges;
     while(n_unknown_messages > 0) {
-        typename Traits::vertex_iterator vi, viend;
-        boost::tie(vi, viend) = boost::vertices(graph);
-        for(; vi != viend; ++vi) {
+        typename Traits::vertex_iterator vi, vi_end;
+        boost::tie(vi, vi_end) = boost::vertices(graph);
+        for(; vi != vi_end; ++vi) {
             unknown_in_edges.clear();
             known_in_edges.clear();
             unknown_out_edges.clear();
@@ -75,7 +171,6 @@ void sum_product(const Graph& graph,
                         *it,
                         known_in_edges.begin(),
                         known_in_edges.end(),
-                        *vi,
                         graph);
                     n_unknown_messages--;
                 }
@@ -99,13 +194,25 @@ void sum_product(const Graph& graph,
                         *message_it,
                         known_in_edges.begin(),
                         known_in_edges.end(),
-                        *vi,
                         graph);
                     n_unknown_messages--;
                 }
             }
         }
     }
+    // make belief
+    typename Traits::vertex_iterator vi, vi_end;
+    boost::tie(vi, vi_end) = boost::vertices(graph);
+    for(; vi != vi_end; ++vi) {
+        known_in_edges.clear();
+        typename Traits::in_edge_iterator ein, ein_end;
+        boost::tie(ein, ein_end) =
+            boost::in_edges(*vi, graph);
+        for(; ein != ein_end; ++ein) {
+            known_in_edges.push_back(*ein);
+        }
+    }
 }
+*/
 
 } // namespace bp
--- testings/graph_test.cpp
+++ testings/graph_test.cpp
@@ -7,36 +7,61 @@
 
 #include <bp/graph.hpp>
 
-using namespace std;
 using namespace boost;
+
+typedef property<vertex_belief_t, int> VertexProperty;
+typedef property<edge_message_t, int> EdgeProperty;
 
 typedef adjacency_list<
             vecS, vecS, bidirectionalS,
-            no_property,
-            property<edge_index_t, std::size_t> > Graph;
+            VertexProperty, EdgeProperty> Graph;
 typedef graph_traits<Graph> Traits;
 typedef Traits::edge_iterator EdgeIterator;
 typedef Traits::edge_descriptor Edge;
-typedef property_map<Graph, edge_index_t>::type EdgeIndexMap;
 
 struct test_visitor {
-    template<typename Message, typename Edge,
-             typename MessageIterator,
-             typename Vertex, typename Graph>
-    void make_message(Message& target, const Edge& edge,
-                      MessageIterator begin,
-                      MessageIterator end,
-                      const Vertex& vertex, const Graph& graph) {
-        target = Message(1);
+
+    template<typename Graph>
+    void init_messages(Graph& graph) {
+        typedef graph_traits<Graph> Traits;
+        typedef typename Traits::vertex_descriptor Vertex;
+        typedef typename Traits::edge_descriptor Edge;
+
+        typename Traits::edge_iterator ei, ei_end;
+        tie(ei, ei_end) = edges(graph);
+        for(; ei != ei_end; ++ei) {
+            put(edge_message, graph, *ei, -1);
+        }
+    }
+
+    template<typename Edge, typename Graph>
+    bool is_initialized(const Edge& edge, const Graph& graph) {
+        return (get(edge_message, graph, edge) > 0);
+    }
+
+    template<typename Edge, typename EdgeIterator, typename Graph>
+    void make_message(const Edge& message,
+                      EdgeIterator in_begin,
+                      EdgeIterator in_end,
+                      Graph& graph) {
+        put(edge_message, graph, message, 1);
         std::cout << "message:"
-                  << boost::get(boost::vertex_index,
-                                graph,
-                                boost::source(edge, graph))
+                  << get(vertex_index,
+                         graph,
+                         boost::source(message, graph))
                   << " -> "
-                  << boost::get(boost::vertex_index,
-                                graph,
-                                boost::target(edge, graph))
+                  << get(vertex_index,
+                         graph,
+                         target(message, graph))
                   << std::endl;
+    }
+
+    template<typename Vertex, typename EdgeIterator, typename Graph>
+    void make_belief(const Vertex& target,
+                     EdgeIterator in_begin,
+                     EdgeIterator in_end,
+                     Graph& graph) {
+        // do nothing
     }
 };
 
@@ -50,12 +75,11 @@ TEST(graphTest, construction)
         add_vertex(graph);
     }
     for(int i=0; i<max_n_vertex-1; ++i) {
-        add_edge(i, i+1, 2*i, graph);
-        add_edge(i+1, i, 2*i + 1, graph);
+        add_edge(i, i+1, graph);
+        add_edge(i+1, i, graph);
     }
 
-    int belief[max_n_vertex];
-    bp::sum_product<int>(graph, belief, test_visitor());
+    bp::sum_product(graph, test_visitor());
 
     ASSERT_TRUE(1 == 1);
 }
