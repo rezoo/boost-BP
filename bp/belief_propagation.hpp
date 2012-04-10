7c876e4ebff825cec1af5460a30d11018793a073
Masaki Saito <rezoolab@gmail.com>
2012-04-10 22:59:16 +0900

    use BFS Algorithm

--- bp/belief_propagation.hpp
+++ bp/belief_propagation.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <vector>
+#include <queue>
+#include <iterator>
 #include <boost/graph/graph_traits.hpp>
 #include <boost/property_map/vector_property_map.hpp>
 #include "properties.hpp"
@@ -30,77 +32,92 @@ void belief_propagation(const Graph& graph, Visitor visitor,
         initialized_map[boost::get(boost::edge_index, graph, *ei)] = 0;
     }
 
+    std::queue<Vertex> que;
+    typename Traits::vertex_iterator vi, vi_end;
+    boost::tie(vi, vi_end) = boost::vertices(graph);
+    for(; vi != vi_end; ++vi) {
+        typename Traits::in_edge_iterator ein, ein_end;
+        boost::tie(ein, ein_end) = boost::in_edges(*vi, graph);
+        std::size_t n_in_edges = std::distance(ein, ein_end);
+        if(n_in_edges == 0) {
+            que.push(*vi);
+        } else if(n_in_edges == 1) {
+            typename Traits::out_edge_iterator eout, eout_end;
+            boost::tie(eout, eout_end) = boost::out_edges(*vi, graph);
+            for(; eout != eout_end; ++eout) {
+                if(boost::target(*eout, graph) == boost::source(*ein, graph)) {
+                    que.push(*vi);
+                }
+            }
+        }
+    }
+
     // make messages
-    EdgesSizeType n_unknown_messages = boost::num_edges(graph);
     typedef std::vector<Edge> EdgeVector;
     typedef typename EdgeVector::iterator EdgeIterator;
     EdgeVector unknown_in_edges;
     EdgeVector known_in_edges;
     EdgeVector unknown_out_edges;
-    while(n_unknown_messages > 0) {
-        typename Traits::vertex_iterator vi, vi_end;
-        boost::tie(vi, vi_end) = boost::vertices(graph);
-        for(; vi != vi_end; ++vi) {
-            unknown_in_edges.clear();
-            known_in_edges.clear();
-            unknown_out_edges.clear();
+    while(que.size()) {
+        Vertex v = que.front(); que.pop();
+        unknown_in_edges.clear();
+        known_in_edges.clear();
+        unknown_out_edges.clear();
 
-            typename Traits::in_edge_iterator ein, ein_end;
-            boost::tie(ein, ein_end) = boost::in_edges(*vi, graph);
-            for(; ein != ein_end; ++ein) {
-                if(initialized_map[
-                        boost::get(boost::edge_index, graph, *ein)] == 1) {
-                    known_in_edges.push_back(*ein);
-                } else {
-                    unknown_in_edges.push_back(*ein);
-                }
+        typename Traits::in_edge_iterator ein, ein_end;
+        boost::tie(ein, ein_end) = boost::in_edges(v, graph);
+        for(; ein != ein_end; ++ein) {
+            if(initialized_map[
+                    boost::get(boost::edge_index, graph, *ein)] == 1) {
+                known_in_edges.push_back(*ein);
+            } else {
+                unknown_in_edges.push_back(*ein);
             }
-            typename Traits::out_edge_iterator eout, eout_end;
-            boost::tie(eout, eout_end) = boost::out_edges(*vi, graph);
-            for(; eout != eout_end; ++eout) {
-                if(initialized_map[
-                        boost::get(boost::edge_index, graph, *eout)] == 0)
-                    unknown_out_edges.push_back(*eout);
-            }
+        }
+        typename Traits::out_edge_iterator eout, eout_end;
+        boost::tie(eout, eout_end) = boost::out_edges(v, graph);
+        for(; eout != eout_end; ++eout) {
+            if(initialized_map[
+                    boost::get(boost::edge_index, graph, *eout)] == 0)
+                unknown_out_edges.push_back(*eout);
+        }
 
-            if(unknown_in_edges.size() == 0) {
-                EdgeIterator it = unknown_out_edges.begin();
-                EdgeIterator it_end = unknown_out_edges.end();
-                for(; it != it_end; ++it) {
+        if(unknown_in_edges.size() == 0) {
+            EdgeIterator it = unknown_out_edges.begin();
+            EdgeIterator it_end = unknown_out_edges.end();
+            for(; it != it_end; ++it) {
+                visitor.make_message(
+                    *it,
+                    known_in_edges.begin(),
+                    known_in_edges.end(),
+                    message_map,
+                    graph);
+                initialized_map[
+                    boost::get(boost::edge_index, graph, *it)] = 1;
+                que.push(boost::target(*it, graph));
+            }
+        } else if(unknown_in_edges.size() == 1) {
+            Vertex target = boost::source(unknown_in_edges[0], graph);
+            EdgeIterator it = unknown_out_edges.begin();
+            EdgeIterator it_end = unknown_out_edges.end();
+            for(; it != it_end; ++it) {
+                if(target == boost::target(*it, graph)) {
                     visitor.make_message(
                         *it,
                         known_in_edges.begin(),
                         known_in_edges.end(),
                         message_map,
                         graph);
-                    n_unknown_messages--;
                     initialized_map[
                         boost::get(boost::edge_index, graph, *it)] = 1;
+                    que.push(boost::target(*it, graph));
+                    break;
                 }
-            } else if(unknown_in_edges.size() == 1) {
-                Vertex target = boost::source(unknown_in_edges[0], graph);
-                EdgeIterator it = unknown_out_edges.begin();
-                EdgeIterator it_end = unknown_out_edges.end();
-                for(; it != it_end; ++it) {
-                    if(target == boost::target(*it, graph)) {
-                        visitor.make_message(
-                            *it,
-                            known_in_edges.begin(),
-                            known_in_edges.end(),
-                            message_map,
-                            graph);
-                        n_unknown_messages--;
-                        initialized_map[
-                            boost::get(boost::edge_index, graph, *it)] = 1;
-                        break;
-                    }
-                }
-            } // end if statement
-        } // end vertex loop
+            }
+        } // end if statement
     } // end message loop
 
     // make beliefs
-    typename Traits::vertex_iterator vi, vi_end;
     boost::tie(vi, vi_end) = boost::vertices(graph);
     for(; vi != vi_end; ++vi) {
         typename Traits::in_edge_iterator ein, ein_end;
